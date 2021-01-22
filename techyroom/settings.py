e917970a46c905e6d783897be179066f9e791950
sirDonger <ssrko98@gmail.com>
2021-01-22 21:27:24 +0200

    docker config

--- techyroom/settings.py
+++ techyroom/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'techyroom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'techyroom',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306'
     }
 }
 
