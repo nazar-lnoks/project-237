a6a9d1e1d539a4a220dbec24a71ec551d80c0efc
ftbillionaire <mysholivsky2003@gmail.com>
2021-01-31 19:04:13 +0200

    userprofile add-sidebar

--- static/js/dropicon.js
+++ static/js/dropicon.js
@@ -1,12 +1,12 @@
-// var icon = document.getElementById('userIcon');
-// var list = document.getElementById('profileList');
+var icon = document.getElementById('userIcon');
+var list = document.getElementById('profileList');
 
-// list.style.display = 'none';
+list.style.display = 'none';
 
-// icon.addEventListener('click', function(){
-//     if(list.style.display === 'none'){
-//         list.style.display = 'block';
-//     } else {
-//         list.style.display = 'none';
-//     }
-// })
+icon.addEventListener('click', function(){
+    if(list.style.display === 'none'){
+        list.style.display = 'block';
+    } else {
+        list.style.display = 'none';
+    }
+})
